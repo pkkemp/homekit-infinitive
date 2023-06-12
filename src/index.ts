5b74d2af2fec2b8ebbcf33d8acefdc089eda9a67
Preston Kemp <pkkemp@users.noreply.github.com>
2023-06-11 23:01:39 -0400

    more initial config

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform';
+import {InfinitiveThermostatAccessory} from "./platformAccessory";
 
 /**
  * This method registers the platform with Homebridge
  */
-export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+// export = (api: API) => {
+//   api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+// };
+
+module.exports = (api) => {
+  api.registerAccessory(PLATFORM_NAME, InfinitiveThermostatAccessory);
 };
